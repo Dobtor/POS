be454ae2fb8893178c7074d8a07224e712385dfc
boming <boming0529@gmail.com>
2019-05-10 11:22:42 +0800

    [Add] dobtor_sale_promotion : first time commit

---
+++ dobtor_sale_promotion/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "dobtor_sale_promotion",
+
+    'summary': """
+        Promotion Rule
+    """,
+
+    'description': """
+        Promotion Rule
+            - BOGO Offer (Buy One Get One Free Rule)
+            - Clubbed Promotion (Merge two promotional offers to provide additional discounts)
+            - Rebate Discount (Set discounts based on Price Range)
+    """,
+
+    'author': "Dobtor SI",
+    'website': "http://www.dobtor.com",
+
+    'category': 'sale',
+    'version': '0.1',
+
+    'depends': [
+        'sale'
+    ],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+}
