76140bc615d35b35cfd12fc83a25eecd1235575b
Steven Wang <wty0512@gmail.com>
2019-05-13 17:44:40 +0800

    [Fix]修正給點數的訂單狀態

--- dobtor_pos_sfic_wallet/models/pos_session.py
+++ dobtor_pos_sfic_wallet/models/pos_session.py
@@ -8,12 +8,12 @@ class PosSession(models.Model):
 
     @api.multi
     def action_pos_session_close(self):
-        self.make_point_transaction()
         super(PosSession, self).action_pos_session_close()
+        self.make_point_transaction()
 
     @api.multi
     def make_point_transaction(self):
         for session in self:
-            orders = session.order_ids.filtered(lambda order: order.state == 'paid')
+            orders = session.order_ids.filtered(lambda order: order.state in ['done', 'invoiced'])
             for order in orders:
                 order.make_point_transaction()
