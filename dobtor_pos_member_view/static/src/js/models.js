d6e0d897dfca31a50745b3db7e2ea54acef7a9d1
kevin.zhu <kevin.zhu@dobtor.com>
2019-05-13 23:20:59 +0800

    [ADD]dobtor_member_view:add member total amount fields

--- dobtor_pos_member_view/static/src/js/models.js
+++ dobtor_pos_member_view/static/src/js/models.js
@@ -4,7 +4,7 @@ odoo.define('dobtor_pos_member_view.models', function (require) {
     var _super_posmodel = models.PosModel.prototype;
     models.PosModel = models.PosModel.extend({
         initialize: function (session, attributes) {
-            models.load_fields('res.partner', ['member_id', 'birthday','expired_date']);
+            models.load_fields('res.partner', ['member_id', 'birthday','expired_date','sfic_point','total_amount']);
             _super_posmodel.initialize.apply(this, arguments);
         },
     });
