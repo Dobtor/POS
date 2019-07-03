b1da0343fdeb198ef162b0c4d2e772530fd04bff
boming <boming0529@gmail.com>
2019-07-03 12:02:25 +0800

    [REM] dobtor_pos_promotion : removing not using code

--- dobtor_pos_promotion/models/pos_order.py
+++ dobtor_pos_promotion/models/pos_order.py
@@ -5,12 +5,4 @@ from odoo import models, fields, api
 class PosOrderLine(models.Model):
     _inherit = 'pos.order.line'
 
-    main_line = fields.Char(
-        string="Main Line"
-    )
-    
-    sub_line = fields.Char(
-        string="Sub Line"
-    )
-    
 
