7897b5eb561c6d58903c2aee642435bfb068f296
boming <boming0529@gmail.com>
2019-07-04 17:56:56 +0800

    [REF] dobtor_pos_multi_pricelist : rename and turn jquery code into lodash

--- dobtor_pos_multi_pricelist/static/src/js/combo_promotion.js
+++ dobtor_pos_multi_pricelist/static/src/js/combo_promotion.js
@@ -16,11 +16,11 @@ odoo.define('dobtor_pos_promotion.combo_promotion', function (require) {
              */
             let group_combo = _.groupBy(combo_list, 'rule_id');
             let result = [];
-            $.each(Object.keys(group_combo), function (i, t) {
+            _.each(Object.keys(group_combo), function (key) {
 
-                let this_rule = group_combo[t][0].rule;
-                let group_variant = _.groupBy(_.filter(group_combo[t], (item) => !!item.marge_tag.length), 'marge_tag');
-                let group_product = _.groupBy(_.filter(group_combo[t], (item) => !!item.marge_product.length), 'marge_product');
+                let this_rule = group_combo[key][0].rule;
+                let group_variant = _.groupBy(_.filter(group_combo[key], (item) => !!item.marge_tag.length), 'marge_tag');
+                let group_product = _.groupBy(_.filter(group_combo[key], (item) => !!item.marge_product.length), 'marge_product');
                 let group_all = _.extend(group_variant, group_product);
 
                 // check system log
@@ -33,32 +33,28 @@ odoo.define('dobtor_pos_promotion.combo_promotion', function (require) {
                 }
 
                 if (Object.keys(group_all).length && Object.keys(group_all).length == self.inner_join_combo_product(this_rule, self.pos).length) {
-
-                    let group_min_qty = _.min(_.map(group_all, (value) => {
-                        return _.size(value);
-                    }));
-
+                    let group_min_qty = _.min(_.map(group_all, (value) => _.size(value)));
                     if (group_min_qty > 0) {
-                        $.each(Object.keys(group_all), function (j, product_group_name) {
+                        _.each(Object.keys(group_all), function (product_group_name) {
                             let product_group_order_by_price = _.chain(group_all[product_group_name])
                                 .sortBy('price');
                             product_group_order_by_price = this_rule.combo_order_by_pirce === 'desc' ? product_group_order_by_price.reverse().value() : product_group_order_by_price.value();
-                            $.each(product_group_order_by_price, function (k, items) {
-                                if (k < group_min_qty) {
-                                    let price = items.product.line_price;
+                            _.each(product_group_order_by_price, function (item, index) {
+                                if (index < group_min_qty) {
+                                    let price = item.product.line_price;
                                     let discount = 0;
-                                    if (items.combo_promotion.based_on === 'price') {
-                                        price = items.combo_promotion.based_on_price - items.product.line_price
-                                        discount = round_pr((((items.product.line_price - items.combo_promotion.based_on_price) / items.product.line_price) * 100.00), 0.01);
-                                    } else if (items.combo_promotion.based_on === 'percentage') {
-                                        price = -round_pr(items.product.line_price * (items.combo_promotion.based_on_percentage / 100), 1)
-                                        discount = items.combo_promotion.based_on_percentage;
+                                    if (item.combo_promotion.based_on === 'price') {
+                                        price = item.combo_promotion.based_on_price - item.product.line_price
+                                        discount = round_pr((((item.product.line_price - item.combo_promotion.based_on_price) / item.product.line_price) * 100.00), 0.01);
+                                    } else if (item.combo_promotion.based_on === 'percentage') {
+                                        price = -round_pr(item.product.line_price * (item.combo_promotion.based_on_percentage / 100), 1)
+                                        discount = item.combo_promotion.based_on_percentage;
                                     }
 
                                     result.push({
                                         rule: this_rule,
-                                        rule_id: t,
-                                        product: items.product,
+                                        rule_id: key,
+                                        product: item.product,
                                         price: price,
                                         quantity: 1,
                                         discount: discount,
