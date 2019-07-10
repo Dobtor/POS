18c55b5d04ec6df370f12624bea964841e164a50
kevin.zhu <kevin.zhu@dobtor.com>
2019-07-10 09:53:40 +0800

    [add]dobtor_pos_member_gender:detach from pos_member_view

---
+++ dobtor_pos_member_gender/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "dobtor_pos_member_gender",
+
+    'summary': """
+       Add member gender info in POS UI """,
+
+    'description': """
+        Add member gender info in POS UI
+    """,
+
+    'author': "Dobtor SI",
+    'website': "http://www.dobtor.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'dobtor_pos_sfic_wallet',
+        'point_of_sale',
+        'dobtor_sale_member',
+        'pos_orders_history',
+        'dobtor_pos_member_view',
+        ],
+        # always loaded
+    'data': [
+        'views/assets.xml',
+
+    ],
+    'qweb': ['static/src/xml/pos.xml'],
+}
