65c0143e004d37844118e538774aa91714966824
boming <boming0529@gmail.com>
2020-03-26 14:54:23 +0800

    [REF] dobtor_pos_member_view : refactor pos member view

--- dobtor_pos_member_view/__manifest__.py
+++ dobtor_pos_member_view/__manifest__.py
@@ -11,23 +11,15 @@
 
     'author': "Dobtor SI",
     'website': "http://www.dobtor.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': [
-        'dobtor_pos_sfic_wallet',
         'point_of_sale',
-        'dobtor_sale_member',
-        ],
-        # always loaded
+        'dobtor_sfic_member',
+    ],
+    # always loaded
     'data': [
         'views/assets.xml',
-        'views/res_partner.xml',
     ],
     'qweb': ['static/src/xml/pos.xml'],
 }
