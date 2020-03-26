931154f1271c2c4c0acc71345dded8fdcb4b8c43
boming <boming0529@gmail.com>
2020-03-26 17:24:56 +0800

    [Fix] dobtor_pos_member_view : fix mobile and phone query bug

--- dobtor_pos_member_view/static/src/js/db.js
+++ dobtor_pos_member_view/static/src/js/db.js
@@ -8,10 +8,10 @@ odoo.define('dobtor_pos_member_view.db', function (require) {
         _partner_search_string: function(partner){
             var str =  partner.name;
             if(partner.phone){
-                str += '|' + partner.phone.split(' ').join('');
+                str += '|' + partner.phone.replace(/[\[\]\(\)\+\*\?\.\-\!\&\^\$\|\~\_\{\}\:\,\\\/]/g, '').split(' ').join('');
             }
             if(partner.mobile){
-                str += '|' + partner.mobile.split(' ').join('');
+                str += '|' + partner.mobile.replace(/[\[\]\(\)\+\*\?\.\-\!\&\^\$\|\~\_\{\}\:\,\\\/]/g, '').split(' ').join('');
             }
             if(partner.barcode){
                 str += '|' + partner.barcode.split(' ').join('');
