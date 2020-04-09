639e58d8bfe50e3a88691022c7998ef80c694fd7
boming <boming0529@gmail.com>
2020-04-09 14:51:42 +0800

    [Fix] dobtor_pos_member_view : fix membership to right fields

--- dobtor_pos_member_view/static/src/js/models.js
+++ dobtor_pos_member_view/static/src/js/models.js
@@ -2,10 +2,21 @@ odoo.define('dobtor_pos_member_view.models', function (require) {
     "use strict";
     var models = require('point_of_sale.models');
     var _super_posmodel = models.PosModel.prototype;
+
     models.PosModel = models.PosModel.extend({
         initialize: function (session, attributes) {
-            models.load_fields('res.partner', ['member_type_name', 'birthday','expired_date','sfic_point','total_amount','gender']);
+            models.load_fields('res.partner', ['birthday', 'expired_date', 'sfic_point', 'total_amount', 'gender', 'member_id']);
             _super_posmodel.initialize.apply(this, arguments);
         },
+    });
+
+    models.load_models([{
+        model: 'sales.member',
+        fields: ['name'],
+        loaded: function (self, member_id) {
+            self.member_id = member_id;
+        },
+    }], {
+        'after': 'res.partner'
     });
 })
